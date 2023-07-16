f7f6749b6e37fcf9339a4ff9fb5198e00221a0ec
sooyon lee <kwlsy666@naver.com>
2023-07-16 22:27:27 +0900

    [스프링부트 회원 프로젝트] 06_로그인 하기(springboot, mysql, jpa, login)

--- src/main/java/com/codingrecipe/member/controller/MemberController.java
+++ src/main/java/com/codingrecipe/member/controller/MemberController.java
@@ -1,18 +1,30 @@
 package com.codingrecipe.member.controller;
 
+import com.codingrecipe.member.dto.MemberDTO;
+import com.codingrecipe.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpSession;
+import java.security.AuthProvider;
+
 @Controller
+@RequiredArgsConstructor
 public class MemberController {
+
+    // 생성자 주입
+    private final MemberService memberService;
 
     // 회원가입 페이지 출력 요청
     @GetMapping("/member/save")
     public String saveForm(){
         return "save";
     }
+
 
     @PostMapping("/member/save")
     public String save(@RequestParam("memberEmail") String memberEmail,
@@ -23,4 +35,37 @@ public class MemberController {
 
         return  "index";
     }
+
+    /*
+    @PostMapping("/member/save")
+    public String save(@ModelAttribute MemberDTO memberDTO) {
+        System.out.println("MemberController.save");
+        System.out.println("memberDTO = " + memberDTO);
+        return  "login";
+    }
+
+     */
+
+    @GetMapping("/member/login")
+    public String loginForm(){
+        return "login";
+    }
+
+    @PostMapping("/member/login")
+    public String login(@ModelAttribute MemberDTO memberDTO, HttpSession session){
+
+        System.out.println("memberDTO = " + memberDTO + ", session = " + session);
+
+        MemberDTO loginResult = memberService.login(memberDTO);
+
+        if(loginResult != null) {
+            // login 성공
+            session.setAttribute("loginEmail", loginResult.getMemberEmail());
+            return "main";
+        }else{
+            // login 실패
+            return "login";
+        }
+    }
+
 }
