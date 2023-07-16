f97b16014a2703ae9fb8324a953fe0ee94e35c77
sooyon lee <kwlsy666@naver.com>
2023-07-16 16:13:37 +0900

    문서

--- src/main/java/com/codingrecipe/member/controller/MemberController.java
+++ src/main/java/com/codingrecipe/member/controller/MemberController.java
@@ -2,6 +2,7 @@ package com.codingrecipe.member.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
@@ -12,5 +13,9 @@ public class MemberController {
         return "save";
     }
 
-
+    @PostMapping("/member/save")
+    public String save() {
+        System.out.println("MemberController.save");
+        return  null;
+    }
 }
