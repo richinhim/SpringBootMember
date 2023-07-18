f632db13368a958cde699d80326f513c91dc3f20
sooyon lee <kwlsy666@naver.com>
2023-07-18 20:45:38 +0900

    *[스프링부트(SpringBoot) 회원 프로젝트] 12_ajax 이메일 중복체크_입력한 이메일 서버로 보내기(springboot, mysql, jpa, login) https://youtu.be/u0vTKR57sCw?list=PLV9zd3otBRt5ANIjawvd-el3QU594wyx7

--- src/main/java/com/codingrecipe/member/controller/MemberController.java
+++ src/main/java/com/codingrecipe/member/controller/MemberController.java
@@ -122,5 +122,11 @@ public class MemberController {
         session.invalidate();
         return "index";
     }
+    
+    @PostMapping("/member/email-check")
+    public @ResponseBody String emailCheck(@RequestParam("memberEmail") String memberEmail){
+        System.out.println("memberEmail = " + memberEmail);
+        return "체크완료";
+    }
 
 }
